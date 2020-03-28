9326872ad0d7b6636fe773323255624f3e0bdef7
Oleksandr Halanin <alexsanify@gmail.com>
2020-03-28 22:12:01 +0200

    improved bandit game

--- games/bandit.js
+++ games/bandit.js
@@ -1,40 +1,51 @@
 class Bandit {
     constructor() {
-        this.wheelValues = ['💩', '💩', '💩', '🔥','🔥','🔥','🔥','🔥','🔥','❤️','❤️','❤️','❤️','🍆','🍆','🍀'];
+        this.wheelValues = ['💰', '💩', '💩', '💩', '🔥', '🔥', '🔥', '🔥', '🔥', '🔥', '❤️', '❤️', '❤️', '❤️', '🍆', '🍆', '🍀'];
     }
 
-    roll(value, userId, userName, state, api, chatId){
+    roll(value, userId, userName, state, api, chatId) {
         let wheel = [...this.wheelValues, ...this.wheelValues, ...this.wheelValues];
         wheel = wheel.sort(() => Math.random() - 0.5);
 
         let rolls = wheel.slice(0, 3);
 
-        let low = rolls.filter(x => x == '🔥');
-        let mid = rolls.filter(x => x == '❤️');
-        let high = rolls.filter(x => x == '🍆');
-        let top = rolls.filter(x => x == '🍀');
+        let low = rolls.filter(x => x == '🔥' || x == '💰');
+        let mid = rolls.filter(x => x == '❤️' || x == '💰');
+        let high = rolls.filter(x => x == '🍆' || x == '💰');
+        let top = rolls.filter(x => x == '🍀' || x == '💰');
 
         let coeff = 0;
-        if (low.length > 1){
+        if (top.length > 1) {
+            coeff = 1 + (1.2 * top.length);
+        } 
+        
+        if (high.length > 1) {
+            coeff = 1 + (0.9 * high.length);
+        } 
+        
+        if (mid.length > 1) {
+            coeff = 1 + (0.4 * mid.length);
+        } 
+        
+        if (low.length > 1) {
             coeff = 1 + (0.2 * low.length);
         }
-        else if (mid.length > 1){
-            coeff = 1 + (0.4 * mid.length);
-        }
-        else if (high.length > 1){
-            coeff = 1 + (0.9 * high.length);
-        }
-        else if (top.length > 1){
-            coeff = 1 + (1.2 * top.length);
-        }
 
-        let prize = Math.floor(value * coeff); 
+        let bonus = rolls.filter(x => x == '💰').length == 1;
+
+        let prize = Math.floor(value * coeff);
         state.users[userId] += prize;
 
         let resultMessage = "🎱 Бандит 🎱\n";
         resultMessage += `${rolls[0]} | ${rolls[1]} | ${rolls[2]}\n`;
-        if (prize > 0)
+        if (prize > 0) {
             resultMessage += `${userName} выиграл ${prize}`;
+
+            if (bonus) {
+                state.users[userId] += value;
+                resultMessage += `\nБонус за 💰: ${value}`;
+            }
+        }
         else
             resultMessage += "Никто не выиграл";
 
